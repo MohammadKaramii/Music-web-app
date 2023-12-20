b9faa1ab9f55cc2d28f54597e48625e13d261a2e
MohammadKaramii <karameshqi@gmail.com>
2023-12-20 05:23:41 +0330

    add sidebar component and dependencies

---
+++ components/Library.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
+import { FiHeart } from "react-icons/fi";
+import { IoIosArrowForward } from "react-icons/io";
+
+
+
+  const Library = () => {
+  return (
+    <div className="flex flex-col ">
+      <div className="flex items-center justify-between border-b-2 border-gray-500 px-5 py-4 ">
+        <div className="inline-flex items-center gap-x-2">
+          <TbPlaylist size={26} className="text-neutral-400" />
+          <p className="text-neutral-400 text-md font-medium">Add Song</p>
+        </div>
+        <AiOutlinePlus
+          size={20}
+          className="text-neutral-400 cursor-pointer  hover:text-white transition"
+        />
+      </div>
+      <div className="flex items-center justify-between border-b-2 px-5 py-4 border-gray-500 ">
+        <div className="inline-flex items-center gap-x-2">
+          <FiHeart size={26} className="text-neutral-400" />
+          <p className="text-neutral-400 text-md font-medium">Liked Song</p>
+        </div>
+        <IoIosArrowForward
+          size={20}
+          className="text-neutral-400 cursor-pointer  hover:text-white transition"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Library;
